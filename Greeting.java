040ab52178095a81aebaafdb43ddfdc5bd09f224
Hans <yunghans@hotmail.com>
2015-02-23 19:38:13 +0800

    Ask for name

--- Greeting.java
+++ Greeting.java
@@ -1,19 +1,28 @@
 import java.util.Calendar;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
 
 public class Greeting{
 
 	public static void main(String args[]){
+		
+		System.out.print("Please enter your name: ");
+
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		String name="";
+		try {name = bf.readLine();} catch (IOException ex) {};
 		
 		Calendar now = Calendar.getInstance();
 		
 		int hour = now.get(Calendar.HOUR_OF_DAY);
 		
 		if (hour < 12) {
-			System.out.println("good morning!");			
+			System.out.println("good morning " + name + "!");			
 		} else if (hour >=12 && hour <19) {
-			System.out.println("good afternoon!");
+			System.out.println("good afternoon " + name + "!");
 		} else if (hour >=19){
-			System.out.println("good evening!");
+			System.out.println("good evening " + name + "!");
 		}
 	}
   
