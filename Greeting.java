46c70f553651a1ac56738548273688c4e91e5267
Hans <yunghans@hotmail.com>
2015-02-23 14:57:31 +0800

    Greet based on the time of the day

--- Greeting.java
+++ Greeting.java
@@ -1,7 +1,20 @@
+import java.util.Calendar;
+
 public class Greeting{
 
-   public static void main(String args[]){
-      System.out.println("good day!");
-   }
+	public static void main(String args[]){
+		
+		Calendar now = Calendar.getInstance();
+		
+		int hour = now.get(Calendar.HOUR_OF_DAY);
+		
+		if (hour < 12) {
+			System.out.println("good morning!");			
+		} else if (hour >=12 && hour <19) {
+			System.out.println("good afternoon!");
+		} else if (hour >=19){
+			System.out.println("good evening!");
+		}
+	}
   
 }
